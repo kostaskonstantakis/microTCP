64e9babac439a1fbacb3fb983de5e306ec8ff768
root <root@DESKTOP-97T1LB4.localdomain>
2020-11-28 22:12:23 +0200

    first working version

--- lib/microtcp.h
+++ lib/microtcp.h
@@ -25,6 +25,9 @@
 #include <sys/socket.h>
 #include <stdint.h>
 
+#include <arpa/inet.h>
+
+
 /*
  * Several useful constants
  */
@@ -104,8 +107,52 @@ typedef struct
 } microtcp_header_t;
 
 
+
+
+// IPv4 AF_INET sockets:
+
+//struct in_addr {
+  //  unsigned long s_addr;          // load with inet_pton()
+//};
+
+////////
+//struct sockaddr_in {
+  //  short            sin_family;   // e.g. AF_INET, AF_INET6
+//    unsigned short   sin_port;     // e.g. htons(3490)
+//    struct in_addr   sin_addr;     // see struct in_addr, below
+//    char             sin_zero[8];  // zero this if you want to
+//};
+
+//struct in_addr {
+//    unsigned long s_addr;          // load with inet_pton()
+//};
+
+//struct sockaddr {
+//    unsigned short    sa_family;    // address family, AF_xxx
+//    char              sa_data[14];  // 14 bytes of protocol address
+//};
+
+
+
+///////////////////////
+
+
+
+
+
+
+
+
+
 microtcp_sock_t
 microtcp_socket (int domain, int type, int protocol);
+
+
+//////////////////////
+//struct sockaddr_in server;
+
+/////////////////////
+
 
 int
 microtcp_bind (microtcp_sock_t *socket, const struct sockaddr *address,
